0bb4371c97971f4ae93bd19e11f1766e65ae8798
m.dmitriev <makc-dmv@yandex.ru>
2018-07-25 14:02:26 +0500

    страница заказов

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,33 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12 >
-        <h1>orders</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Orders</h1>
+        <v-list two-line subheader>
+          <v-list-tile
+            avatar
+            v-for="order in orders"
+            :key="order.id"
+            >
+            <v-list-tile-action>
+              <v-checkbox
+                @change="markDone(order)"
+                :input-value="order.done"
+                color="success"
+                ></v-checkbox>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ order.name }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ order.phone }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn
+              :to="'/ad/' + order.adId"
+              class="primary"
+              >Open</v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -10,7 +35,22 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      orders: [
+        {
+          id: 'wef',
+          name: 'пупкин',
+          phone: '+7xxxxxxxxxx',
+          adId: '123',
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+    markDone (order) {
+      order.done = true
+    }
   }
 }
 </script>
