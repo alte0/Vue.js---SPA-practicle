5fd5a0095e8b995feafb5aa2400f25feb5a33b74
m.dmitriev <makc-dmv@yandex.ru>
2018-07-25 13:43:28 +0500

    главная страница

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,9 @@
     </v-navigation-drawer>
     <v-toolbar app dark color="primary">
       <v-toolbar-side-icon v-on:click="drawer = !drawer"  class="hidden-md-and-up"></v-toolbar-side-icon>
-      <v-toolbar-title>Ad aplication</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" class="pointer">Ad aplication</router-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn flat v-for="link in links"  v-bind:key="link.title" v-bind:to="link.url">
@@ -47,3 +49,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+</style>
